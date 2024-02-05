cfa55e673f95ef8fc4a35fdf2b51fd3626cb92bd
efeichorn <75636656+efeichorn@users.noreply.github.com>
2024-02-05 10:32:02 -0500

    Update arrowscroll.js

--- arrowscroll.js
+++ arrowscroll.js
@@ -1,47 +1,47 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-  var slideIndex = 0; // Start with the first slide (index 0)
-  showSlides();
+var slideIndex = 1;
+showSlides(slideIndex);
 
-  function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-
-    for (i = 0; i < slides.length; i++) {
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
-    }
-
-    slideIndex++;
-
-    if (slideIndex > slides.length) {
-      slideIndex = 1; // Reset to the first slide if we reach the end
-    }
-
-    for (i = 0; i < dots.length; i++) {
+  }
+  for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
-    }
-
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-
-    // Change slide every 2000 milliseconds (2 seconds)
-    setTimeout(showSlides, 2000);
   }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
 
-  // Next/previous controls with arrows
-  function plusSlides(n) {
-    slideIndex += n;
-    showSlides();
-  }
+// Next/previous controls with arrows
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-  document.onkeydown = function (e) {
-    e = e || window.event;
-    if (e.keyCode == '37') {
-      plusSlides(-1); //left <- show Prev image
-    } else if (e.keyCode == '39') {
-      // right -> show next image
-      plusSlides(1);
+    document.onkeydown = function(e) {
+        e = e || window.event;
+        if (e.keyCode == '37') {
+            plusSlides(-1) //left <- show Prev image
+        } else if (e.keyCode == '39') {
+            // right -> show next image
+            plusSlides(1)
+        }
     }
-  };
 });
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
 
